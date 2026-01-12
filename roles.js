21578f2b08523bf5bde2edd42c5a405e492e5dc1
Hamzaamadou <154360201+Hamzaamadou@users.noreply.github.com>
2026-01-12 17:59:48 +0100

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,23 +1,13 @@
-// backend/utils/roles.js
+module.exports.requireRole = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !req.user.role) {
+      return res.status(403).json({ message: "Accès refusé" });
+    }
 
-module.exports.isAdmin = (req, res, next) => {
-  // Si tu utilises un middleware JWT avant,
-  // req.user sera déjà rempli
-  if (!req.user) {
-    return res.status(401).json({ message: "Non authentifié" });
-  }
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Rôle non autorisé" });
+    }
 
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Accès refusé (admin seulement)" });
-  }
-
-  next();
+    next();
+  };
 };
-
-const { requireRole } = require("../utils/roles");
-
-router.get("/admin/orders", requireRole("admin","superadmin"), ... );
-
-router.post("/agent/validate", requireRole("agent","admin","superadmin"), ...);
-
-
