3f3d0a8d67b87c09bfdec9de37de9ee04d6ab8dd
Hamzaamadou <154360201+Hamzaamadou@users.noreply.github.com>
2026-01-13 03:17:47 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
+// backend/db.js
 const mongoose = require("mongoose");
 
-module.exports = async function connectDB() {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connecté");
-  } catch (err) {
-    console.error("❌ MongoDB erreur :", err);
+    console.log("MongoDB Atlas connecté ✅");
+  } catch (error) {
+    console.error("Erreur MongoDB ❌", error.message);
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
